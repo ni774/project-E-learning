f88156b65080efb936238974e017780ec7a836bf
ni774 <gnikhil556@gmail.com>
2022-06-27 16:37:16 +0530

    changes made on way of db connection using mongoose before it was 'mongoClient' from mongoDb

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,26 +1,11 @@
-const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
+
+const mongoose = require('mongoose');
+const db = process.env.ATLAS_URI;
+mongoose.connect(db,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
- 
-var _db;
- 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("Course");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
-  },
- 
-  getDb: function () {
-    return _db;
-  },
-};
+}).then(()=>{
+  console.log("connection successfull from databse");
+}).catch((e)=>{
+     console.log(e);
+})
--- server/server.js
+++ server/server.js
@@ -22,11 +22,6 @@ app.use(express.json());
 const dbo = require("./db/conn");
  
 app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
   console.log(`Server is running on port: ${port}`);
 });
-
+  
